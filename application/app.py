6d6320c74a055e1acf30407749ab8098d7c0ccdf
Artem Klopov <artemklopow@gmail.com>
2021-04-23 09:38:18 +0300

    modified gitignore, add pie

--- application/app.py
+++ application/app.py
@@ -4,10 +4,21 @@ import dash_core_components as dcc
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 from collections import Counter
 
+
+def get_top3_specialty(string):
+    """
+    Принимает строку из слов разделенных пробелом.
+    Возвращает 3 наиболее часто встречающихся слова разделенных заяпятой.
+    Значаения 'nan' удаляются
+    """
+    specialties = filter(lambda x: x != 'nan', string.split(' '))
+    count_specialties = Counter(specialties)
+    return ', '.join([i[0] for i in count_specialties.most_common(3)])
 
 # Основной датасет.
 df = pd.read_csv('application/data/df.csv', sep=',')
@@ -23,17 +34,10 @@ months = dict((i, {'timestamp': months[i], 'string': months[i].strftime('%b %Y')
 payers = list(df['PAYER'].unique())
 
 # Группировки датасета. Чтобы не делать это в callback
-df_sum_paid_by_payer = df.groupby(by=['MONTH', 'PAYER'], as_index=False).agg({'PAID_AMOUNT': 'sum'})
-
-def get_top3_specialty(string):
-    """
-    Принимает строку из слов разделенных ';'.
-    Возвращает 3 наиболее часто встречающихся слова разделенных заяпятой.
-    Значаения 'nan' удаляются
-    """
-    specialties = filter(lambda x: x != 'nan', string.split(';'))
-    count_specialties = Counter(specialties)
-    return ', '.join([i[0] for i in count_specialties.most_common(3)])
+df_sum_paid_by_payer = df.groupby(by=['MONTH', 'PAYER'], as_index=False).agg({'PAID_AMOUNT': 'sum', 
+                                                                              'CLAIM_SPECIALTY': lambda x: ' '.join(x)})
+df_sum_paid_by_payer.rename(columns={'CLAIM_SPECIALTY': 'TOP_SPECIALTIES'}, inplace=True)
+df_sum_paid_by_payer['TOP_SPECIALTIES'] = df_sum_paid_by_payer['TOP_SPECIALTIES'].apply(lambda x: get_top3_specialty(x))
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
@@ -68,6 +72,7 @@ app.layout = html.Div([
     ),
     dcc.Graph(id='timeseries', style={'border': '3px solid green'}),
     dcc.Graph(id='scatter', style={'border': '3px solid black'}),
+    dcc.Graph(id='pie', style={'border': '3px solid red'}),
     html.Br(),
     html.Div(id='out_date_slider'),
     html.Div(id='out_payers_checklist')
@@ -95,10 +100,13 @@ def update_timeseries(dates, payers):
     min_date = months[dates[0]]['timestamp']
     max_date = months[dates[1]]['timestamp']
     filtered_df = df_sum_paid_by_payer[(df_sum_paid_by_payer['MONTH'] >= min_date) & (df_sum_paid_by_payer['MONTH'] <= max_date) & (np.isin(df_sum_paid_by_payer['PAYER'], payers))]
-    fig = px.line(filtered_df, x="MONTH", y="PAID_AMOUNT", line_group='PAYER', color='PAYER')
+    fig = px.line(filtered_df, x="MONTH", y="PAID_AMOUNT", line_group='PAYER', color='PAYER', hover_data=['TOP_SPECIALTIES'])
+
     fig.update_xaxes(
         tickformat="%b\n%Y"
     )
+    fig.update_traces(mode="markers+lines")
+
     return fig
 
 @app.callback(
@@ -109,11 +117,14 @@ def update_scatter(dates, payers):
     min_date = months[dates[0]]['timestamp']
     max_date = months[dates[1]]['timestamp']
     filtered_df = df[(df['MONTH'] >= min_date) & (df['MONTH'] <= max_date) & (np.isin(df['PAYER'], payers))]
-    filtered_df = filtered_df.groupby(['SERVICE_CATEGORY', 'PAYER'], as_index=False).agg({'PAID_AMOUNT': 'sum', 'MONTH': 'count'})
-    filtered_df.rename(columns={'MONTH': 'CLAIMS'}, inplace=True)
-    filtered_df['TOP_SPECIALTIES'] = df.groupby(['SERVICE_CATEGORY', 'PAYER'])['CLAIM_SPECIALTY'].transform(lambda x: ';'.join(x))
-    filtered_df['TOP_SPECIALTIES'] = filtered_df['TOP_SPECIALTIES'].apply(lambda x: get_top3_specialty(x))
-    #filtered_df['PAID_AMOUNT_FAKE_VALUES'] = filtered_df['PAID_AMOUNT'].apply(lambda x: max(x, 1e7))
+    
+    filtered_df = filtered_df.groupby(by=['SERVICE_CATEGORY', 'PAYER'], as_index=False).agg({'PAID_AMOUNT': 'sum',
+                                                                              'MONTH': 'count',
+                                                                              'CLAIM_SPECIALTY': lambda x: ' '.join(x)})
+
+    filtered_df['CLAIM_SPECIALTY'] = filtered_df['CLAIM_SPECIALTY'].apply(lambda x: get_top3_specialty(x))
+    filtered_df.rename(columns={'MONTH': 'CLAIMS', 'CLAIM_SPECIALTY': 'TOP_SPECIALTIES'}, inplace=True)
+    
     fig = px.scatter(filtered_df, x='PAYER', 
                 y='SERVICE_CATEGORY', 
                 color='CLAIMS', 
@@ -122,6 +133,23 @@ def update_scatter(dates, payers):
                 size_max=40)
     return fig
 
+@app.callback(
+    dash.dependencies.Output('pie', 'figure'),
+    [dash.dependencies.Input('date_slider', 'value',),
+    dash.dependencies.Input('payers_checklist', 'value')])
+def update_pie(dates, payers):
+    min_date = months[dates[0]]['timestamp']
+    max_date = months[dates[1]]['timestamp']
+    filtered_df = df[(df['MONTH'] >= min_date) & (df['MONTH'] <= max_date) & (np.isin(df['PAYER'], payers))]
+    filtered_df = filtered_df.groupby(['SERVICE_CATEGORY'], as_index=False).agg({'PAID_AMOUNT': 'sum', 'CLAIM_SPECIALTY':lambda x: ' '.join(x)})
+    filtered_df['CLAIM_SPECIALTY'] = filtered_df['CLAIM_SPECIALTY'].apply(lambda x: get_top3_specialty(x))
+    labels = filtered_df['SERVICE_CATEGORY']
+    values = filtered_df['PAID_AMOUNT']
+    customdata = filtered_df['CLAIM_SPECIALTY']
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, customdata = customdata, hole=.6,
+                            hovertemplate = "SERVICE_CATEGORY:%{label} <br>PAID_AMOUNT: %{value} </br> TOP SPECIALTIES:%{customdata}")])
+   
+    return fig
 
 
 
